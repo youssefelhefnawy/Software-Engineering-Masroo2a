53429b27f63e92112e113601a1ac9f2882903f52
YoussefFekky <34752378+YoussefFekky@users.noreply.github.com>
2018-12-27 13:40:30 +0200

    Update Data_base_handler.java

--- Data_base_handler.java
+++ Data_base_handler.java
@@ -1,17 +1,16 @@
 import java.util.Vector;
 
 public interface Data_base_handler 
-{	public int get_size();
-	
-
+{
+	public int get_size();
 	public Vector<Post> getposts_by_user(String Userid);
-	public Vector<Post> getposts_by_everything(String d); 
+	public Vector<Post> getposts_by_everything(String cat, String loc);
 	public User Getuser(String userid);
 	public User Getuser(String username, String password);
-	public Post getpost(String userid);
+	public Post getpost(String postid);
 	public boolean Insert_user(User Newuser);
 	public boolean Insert_post(Post new_post);
 	public boolean Delete(String ID);
 	public boolean Update_user(User user, String ID);
-	public boolean update_post(Post post);
+	public boolean update_post(Post post, String ID);
 }
